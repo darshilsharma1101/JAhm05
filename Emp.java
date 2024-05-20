7ceb5e69b1e185a7ba1502d2706fc6198be41a21
darshilsharma1101 <169170746+darshilsharma1101@users.noreply.github.com>
2024-05-20 11:30:09 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,15 @@
+package com.studyopedia;
+
+public class Emp {
+
+	public static void main(String[] args) {
+		
+		Employee e1 =new Employee();
+		e1.setEid(25);
+		e1.setEmail("darshil@gmail.com");
+		System.out.println(e1);
+		//System.out.println(e1.getEid()+" "+e1.getEmail());
+
+	}
+
+}
