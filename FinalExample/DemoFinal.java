382230eacef534e51a8ccdfc279035ddcf74f7cf
darshilsharma1101 <169170746+darshilsharma1101@users.noreply.github.com>
2024-06-09 11:58:00 +0530

    Add files via upload

---
+++ FinalExample/DemoFinal.java
@@ -0,0 +1,8 @@
+package finaleexample;
+
+public class DemoFinal {
+public static void main(String[] arg) {
+	FinaleExample fe = new FinaleExample();
+	fe.display();
+}
+}
---
+++ FinalExample/FinaleExample.java
@@ -0,0 +1,15 @@
+package finaleexample;
+final class Test{
+	
+}
+public class FinaleExample {
+	private int score;
+	private final int per = 8;
+	public void display() {
+		int r = per *12;
+		System.out.println(r + per);
+	}
+	public final void SayHello() {
+		System.out.println("WE Learn Final");
+	}
+}
