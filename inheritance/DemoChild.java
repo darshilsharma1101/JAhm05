46eb6d82f3a44c8ea9ae89bec97f35e9e4699a3c
darshilsharma1101 <169170746+darshilsharma1101@users.noreply.github.com>
2024-06-09 12:04:17 +0530

    Add files via upload

---
+++ inheritance/DemoChild.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class DemoChild {
+public static void main(String[] args) {
+		
+
+		ChildClass c = new ChildClass();
+		c.setAmt(12000);
+		System.out.println(c);
+	}
+
+}
